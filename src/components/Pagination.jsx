f8cb631ea50124a71e4a834a69c4ede2a1b10af2
AnalHalder <analhalder02@gmail.com>
2024-08-11 19:06:10 +0530

    #

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -5,7 +5,7 @@ function Pagination() {
   const { allBlogs, startIdx, setStartIdx, endIdx, setEndIdx } = useContext(BlogContext);
   
   return (
-    <div className="flex justify-center mt-4">
+    <div className="flex justify-center mt-2 mb-4">
       <button 
         className="bg-blue-600 hover:bg-blue-800 text-white text-md rounded-md w-32 py-2"
         onClick={() => {
